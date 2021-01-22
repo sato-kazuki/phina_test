073be8795dd2523a4770b2d871933ba1c56bd938
DESKTOP-sk <yomitomiku@gmail.com>
2021-01-22 09:03:15 +0900

    上位ディレクトリから管理に変更

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>公開物置き場</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="">
-</head>
-<body>
-<header>
-    <a href=".">公開物置き場</a>
-</header>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<article>
-<section>
-<p>目次</p>
-<ul>
-    <li><a href="calculator">電卓</a></li>
-    <li><a href="timecard">勤怠記録出力</a></li>
-    <li><a href="bbs">簡易bbs</a></li>
-    <li><a href="upload">画像アップローダ</a></li>
-</ul>
-</section>
-</article>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-<footer>
-    <hr>
-    <a href="">*工事中</a>
-</footer>
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
